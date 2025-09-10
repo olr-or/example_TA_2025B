16c5260ac610a69ebce5611ae855f58c57094612
Hyuna <olr.or@yonsei.ac.kr>
2025-09-10 14:11:42 +0900

    Update

--- 2 week/Fortran_example.f90
+++ 2 week/Fortran_example.f90
@@ -1,23 +1,26 @@
-! Build -> gfortran -g -O0 <file name>.f90 -o <file name>
+! Build -> gfortran -g <file name>.f90 -o <file name>
 
 program lecture_example
   implicit none
+  integer :: ivar, jvar
+  real :: xvar, yvar
+  double precision :: dvar
+  logical :: flag
+  character(len=10) :: name
 
-  ! 1. Overview
+  ivar = 10
+  jvar = 3
+  xvar = 2.5
+  yvar = -7.2
+  dvar = 3.141592653589793D0
+  flag = .true.
+  name = "Fortran"
 
   print*, "=== Fortran Overview ==="
   print*, "This program demonstrates variables,"
   print*, "arithmetic operations, and control statements."
+  print*, "========================="
 
-  ! 2. Variables
-
-  integer :: ivar = 10, jvar = 3
-  real :: xvar = 2.5, yvar = -7.2
-  double precision :: dvar = 3.141592653589793D0
-  logical :: flag = .true.
-  character(len=10) :: name
-
-  name = "Fortran"
   print*
   print*, "=== Variables ==="
   print*, "Integer ivar =", ivar
@@ -26,22 +29,17 @@ program lecture_example
   print*, "Logical flag =", flag
   print*, "Character name =", name
 
-  ! 3. Arithmetic Operations
-
   print*
   print*, "=== Arithmetic Operations ==="
   print*, "ivar + jvar =", ivar + jvar
   print*, "ivar - jvar =", ivar - jvar
   print*, "ivar * jvar =", ivar * jvar
-  print*, "ivar / jvar =", ivar / jvar      ! 정수 나눗셈
+  print*, "ivar / jvar =", ivar / jvar
   print*, "real division (xvar / jvar) =", xvar / jvar
   print*, "ivar ** jvar =", ivar ** jvar
   print*, "sqrt(xvar) =", sqrt(xvar)
   print*, "mod(ivar, jvar) =", mod(ivar, jvar)
 
-  ! 4. Control Statements
-
-  ! (1) IF문
   print*
   print*, "=== Control Statements: IF ==="
   if (xvar > 0.0) then
@@ -50,7 +48,6 @@ program lecture_example
      print*, "xvar is not positive."
   end if
 
-  ! (2) IF-ELSE IF문
   print*
   print*, "=== Control Statements: IF-ELSEIF ==="
   if (ivar > 10) then
@@ -61,7 +58,6 @@ program lecture_example
      print*, "ivar < 10"
   end if
 
-  ! (3) SELECT CASE문
   print*
   print*, "=== Control Statements: SELECT CASE ==="
   select case (jvar)
@@ -73,25 +69,22 @@ program lecture_example
      print*, "jvar is something else"
   end select
 
-  ! (4) 산술 IF문
   print*
-  print*, "=== Control Statements: Arithmetic IF ==="
-  if (ivar - jvar) 100, 200, 300
-100 print*, "ivar - jvar < 0"
-    goto 400
-200 print*, "ivar - jvar == 0"
-    goto 400
-300 print*, "ivar - jvar > 0"
-400 continue
+  print*, "=== Control Statements: Modern IF ==="
+  if (ivar - jvar < 0) then
+     print*, "ivar - jvar < 0"
+  else if (ivar - jvar == 0) then
+     print*, "ivar - jvar == 0"
+  else
+     print*, "ivar - jvar > 0"
+  end if
 
-  ! (5) GOTO문 활용
   print*
   print*, "=== Control Statements: GOTO ==="
   goto 500
   print*, "This line will be skipped."
 500 print*, "Jumped here with GOTO."
 
-  ! (6) STOP문
   print*
   print*, "=== Program End with STOP ==="
   stop
